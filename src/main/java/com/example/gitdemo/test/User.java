302546d7ecba2bd96cc4c06138191e9eb1eb51e1
zhangjun-101 <2464903779@qq.com>
2022-03-25 10:02:35 +0800

    hot1

--- src/main/java/com/example/gitdemo/test/User.java
+++ src/main/java/com/example/gitdemo/test/User.java
@@ -8,4 +8,9 @@ public class User {
     private String name;
     private String sex;
     private int age;
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git2");
+    }
 }
